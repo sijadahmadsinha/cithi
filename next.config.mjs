7259ab6289ba92a5f8829c6885c2a791ae52225e
Sijad <sijadahmadsinha@gmail.com>
2026-06-08 14:15:20 +0600

    Disable Next.js dev indicator badge

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  devIndicators: {
+    buildActivity: false,
+    buildActivityPosition: 'bottom-right',
+  },
 };
 
 export default nextConfig;
